468eba3e4e763c83fc9244f7b168ebfd88c52988
afer92 <11302604+afer92@users.noreply.github.com>
2021-01-27 19:33:39 +0100

    Enhanced tests

--- TestCode.py
+++ TestCode.py
@@ -2,20 +2,26 @@
 from Domoticz import Device
 from Domoticz import Devices
 from Domoticz import Images
-from Domoticz import Parameters
+import Domoticz
 
 # your params
+
 API_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' # Your api key from Climacell
 LATITUDE = '47.111111' # Your location
 LONGITUDE = '2.222222'
 
+
+Parameters = {}
 Parameters['Mode1'] = API_key
 Parameters['Mode2'] = u"{},{}".format(LATITUDE, LONGITUDE)
 Parameters['Mode3'] = 300  # UpdateCycleMeteo
 Parameters['Mode4'] = 1800  # UpdateCycleAirQuality
+Parameters['Mode5'] = 'Debug'  # Debug or Normal
+
 
 def runtest(plugin):
 
+    Domoticz.Start()
     plugin.onStart()
     plugin.onHeartbeat()
     plugin.onHeartbeat()
